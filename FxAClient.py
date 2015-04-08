7ef48e664c39e4781ebeb9b2b993269c24bfff0d
dhanani94 <dhanani94@gmail.com>
2015-04-08 11:23:19 -0400

    working on command line input for fxaclient

--- FxAClient.py
+++ FxAClient.py
@@ -1,34 +1,48 @@
 import sys
-import RxPServer
 
 def main():
 	if len(sys.argv) < 3:
 		usage()
 	else:
-		portNumber = eval(sys.argv[1])
-	s = socket(AF_INET, SOCK_DGRAM)
-	s.bind(('', port))
-	print 'udp echo server ready'
-	addr = connect(s)
-	data, addr = s.recvfrom(BUFSIZE)
-	if(data[:3] == "GET"):
-		fileName = data[4:]
-		send(fileName, s, addr)
+		bindport = eval(sys.argv[1])
+		addr = eval(sys.argv[2]), eval(sys.argv[3])
+	while 1:
+		line = sys.stdin.readline()
+		if not line:
+			break
+		if(line == "connect"):
+			connect()
+		if(line[:3] == "get"):
+			get(line[4:])
+		if (line[:4] == "post"):
+			post(line[4:])
+		if (line == "disconnect"):
+			disconnect()
 
 def usage():
 	sys.stdout = sys.stderr
 	print 'Usage: FxAServer X A P'
-	print 'X: the port number at which the FxA­server’s UDP socket should bind to (odd number) 
+	print 'X: the Even port # for FxASevers socket'
 	print 'A: the IP address of NetEmu'
  	print 'P: the UDP port number of NetEmu'
 	sys.exit(2)
 
 def connect():
+	print("attempting to connect")
+	s = socket(AF_INET, SOCK_DGRAM)
+	s.bind(('', bindport))
+	RxPClient.connect(s, addr)
 
-def get():
+
+def get(cal):
+	print("attempting to get")
 	pass
 
 def post():
+	print("attempting to post")
 	pass
 
 def disconnect():
+	print("attempting to disconnect")
+
+main()
